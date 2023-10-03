b75a9e860ab27316903852492897d4eac0e2d09c
anaaa <145471991+analopss@users.noreply.github.com>
2023-10-03 11:47:09 -0300

    editando comandos

---
+++ sktech.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+   background("purple");
+  }
+  
+  
+  function draw() {
+   stroke("white");
+  fill("blue");
+   if(mouseIsPressed)
+  rect(mouseX,mouseY,20,20);
+  }
